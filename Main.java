9a880dc22021788b7eced1575d2579303121c094
khrid0328 <khrid0328@wrdsb.ca>
2020-10-30 14:47:47 +0000

    Fahrenheit and Celsius degrees conversion

--- Main.java
+++ Main.java
@@ -6,7 +6,18 @@ public class Main implements Runnable, ActionListener{
 
   // Class Variables  
   
+  JPanel mainPanel;
 
+  JLabel firstLabel;
+  JLabel secondLabel;
+
+  JTextField firstInput;
+  JTextField secondInput;
+
+  JButton fToC; 
+  JButton cToF;
+
+  Font biggerText;
 
   // Method to assemble our GUI
   public void run(){
@@ -19,7 +30,69 @@ public class Main implements Runnable, ActionListener{
     // shows the window
     frame.setVisible(true);
  
-    
+    // initialize the main panal
+    mainPanel = new JPanel();
+
+    // disable any layout helpers
+    mainPanel.setLayout(null);
+
+    // create the side labels
+    firstLabel = new JLabel("Degrees Fahrenheit");
+    secondLabel = new JLabel("Degrees Celsius");
+
+    // layout the labels by setting the coordinate and setSize
+    firstLabel.setBounds(40, 40, 250, 30);
+    secondLabel.setBounds(40, 90, 250, 30);
+
+    // INITIALIZE THE INPUT TEXT FIELDS
+    firstInput = new JTextField();
+    secondInput = new JTextField();
+
+    // set the location and setSize
+    firstInput.setBounds(290, 40, 100, 30);
+    secondInput.setBounds(290, 90, 100, 30);
+
+    // initialize the buttons
+    cToF = new JButton("C --> F");
+    fToC = new JButton("F --> C");
+
+    // set the location and set setSize
+    fToC.setBounds(420, 40, 150, 30);
+    cToF.setBounds(420, 90, 150, 30);
+
+    // add an action listener to the buttons
+    fToC.addActionListener(this);
+    cToF.addActionListener(this);
+
+    // set the action commant on the buttons
+    fToC.setActionCommand("F --> C");
+    cToF.setActionCommand("C --> F");
+
+    // add the labels to the main mainPanel
+    mainPanel.add(firstLabel);
+    mainPanel.add(secondLabel);
+
+    // add the text fields to the main mainPanel
+    mainPanel.add(firstInput);
+    mainPanel.add(secondInput);
+
+    // add the buttons to the panel
+    mainPanel.add(cToF);
+    mainPanel.add(fToC);
+
+    // create a bigger font to use
+    biggerText = new Font("arial",Font. PLAIN, 20);
+
+    // set the fonr on the area I want to use
+    firstLabel.setFont(biggerText);
+    secondLabel.setFont(biggerText);
+    firstInput.setFont(biggerText);
+    secondInput.setFont(biggerText);
+    cToF.setFont(biggerText);
+    fToC.setFont(biggerText);
+
+    // add the panel to the window
+    frame.add(mainPanel);
 
   }
 
@@ -28,6 +101,39 @@ public class Main implements Runnable, ActionListener{
     // get the command from the action
     String command = e.getActionCommand();
 
+    if(command.equals("F --> C")){
+      // get the text from the text box
+      String firstText = firstInput.getText();
+
+      // change the string into an integer
+      int degreeF = Integer.parseInt(firstText);
+
+      // callculate the degree in Celsius
+      int temperatureC = (5 * degreeF - 160) / 9;
+
+      // clear the second text box
+      secondInput.setText("");
+
+      // output the answer to the second text box
+      secondInput.setText("" + temperatureC);
+
+    }else if(command.equals("C --> F")){
+      // get the text from the second text box
+      String secondText = secondInput.getText();
+
+      // change the string into an integer
+      int degreeC = Integer.parseInt(secondText);
+
+      // callculate the degree in Celsius
+      int temperatureF = (9 / 5) * degreeC + 32;
+
+      // clear the first text box
+      firstInput.setText("");
+
+      // output the answer to the second text box
+      firstInput.setText(""+ temperatureF);
+    }
+    
   }
 
   // Main method to start our program
